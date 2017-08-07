4930c59e9f402cfb89deef17874b1468b6b58c2b
JoseK21 <josek21nunez@gmail.com>
2017-08-07 10:41:51 -0600

    Lista Doble Enlazada

--- LinkedDB/src/Lists/List_Double.java
+++ LinkedDB/src/Lists/List_Double.java
@@ -1,5 +1,37 @@
 package Lists;
 
 public class List_Double {
+	private NodoList_Double inicio,fin;
+	
+	public List_Double(){
+		inicio=fin=null;	
+	}
+	//Metodo para saber si la lista esta vacia
+	public boolean estaVacia(){
+		return inicio==null;
+	}
+	//metodo para agregar nodos al inicio
+	public void agregarAlInicio(int el){
+		if (!estaVacia()){
+			inicio=new NodoList_Double(el,inicio,null);
+			inicio.siguiente.anterior=inicio;
+		}
+		else{
+			inicio=fin=new NodoList_Double(el);
+		}
+	}
+	//metodo para mostar la lista de inicio a fin
+	public void mostrasLista(){
+		if(!estaVacia()){
+			String datos="<=>";
+			NodoList_Double auxiliar=inicio;
+			
+			while (auxiliar!=null){
+				datos=datos+"["+auxiliar.dato+"]<=>";
+				auxiliar=auxiliar.siguiente;
+			}
+		}
+	}
 
 }
+
--- LinkedDB/src/Lists/NodoList_Double.java
+++ LinkedDB/src/Lists/NodoList_Double.java
@@ -1,5 +1,20 @@
 package Lists;
 
 public class NodoList_Double {
-
+	public int dato;
+	NodoList_Double siguiente;
+	NodoList_Double anterior;
+	
+	//Constructor para cuando aun ni hay nodos
+		public NodoList_Double(int el){
+			this(el,null,null);
+	}
+	//COnstructor para cuando ya haya nodos
+	public NodoList_Double(int el,NodoList_Double s,NodoList_Double a){
+		dato=el;
+		siguiente=s;
+		anterior=a;
+	}
+	
 }
+
