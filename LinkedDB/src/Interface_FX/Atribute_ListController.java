918cfdbc788ea650a2523c51eaec9ecab46d11d0
JoseK21 <josek21nunez@gmail.com>
2017-08-20 01:59:22 -0600

    Visibilidad de botones / accesos bloqueados

--- LinkedDB/src/Interface_FX/Atribute_ListController.java
+++ LinkedDB/src/Interface_FX/Atribute_ListController.java
@@ -3,29 +3,77 @@ package Interface_FX;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class Atribute_ListController extends Application {
 	ObservableList<String> tiposStatusList = FXCollections.observableArrayList("Entero","Flotante","Cadena","Fecha-Hora");
-	
+	ObservableList<String> tiposEspecialesStatusList = FXCollections.observableArrayList("Llave Primaria","Llave Foranea");
 		
+	@FXML private ChoiceBox<String> tiposStatusBox;	
+	@FXML private ChoiceBox<String> tiposEspecialesStatusBox;
+	@FXML private RadioButton textRequerido ;
+	@FXML private Label val_def;
+	@FXML private TextField dataval_def;
+	
+	
+	
 	@FXML
-	private ChoiceBox<String> tiposStatusBox;
+	public void DisableVal_Defaul(ActionEvent event){
+		if (textRequerido.isSelected()){
+			val_def.setDisable(false);
+			dataval_def.setDisable(false);			
+		}else{
+			val_def.setDisable(true);
+			dataval_def.setDisable(true);	
+		}
+	}
+	
+	
+	@FXML
+	public void atras(ActionEvent event){
+		// String valor = tiposStatusBox.getValue();         Forma de pasar valores 
+		// System.out.println("Dato seleccionado :"+valor);
+		
+		try{
+			((Node)event.getSource()).getScene().getWindow().hide();
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Create_DocJSON.fxml"));
+			Parent root1 = (Parent) fxmlLoader.load();
+			Stage stage = new Stage();
+			
+			stage.setTitle("LInkedDB");
+			stage.setScene(new Scene(root1));
+			stage.show();
+		}catch (Exception e){
+			System.out.println("Can´t load new window");
+	}}
+	
 	
 	@FXML
 	private void initialize(){		
 		tiposStatusBox.setValue("Entero");
 		tiposStatusBox.setItems(tiposStatusList);
+		tiposEspecialesStatusBox.setValue("Llave Primaria");
+		tiposEspecialesStatusBox.setItems(tiposEspecialesStatusList);
+		
+		val_def.setDisable(true);
+		dataval_def.setDisable(true);
+		
 	}
+
 	
 	@Override
 	public void start(Stage primaryStage) {
 		
 	}
 
-	public static void main(String[] args) {
-		launch(args);
-	}
 }
--- LinkedDB/src/Interface_FX/Create_DocJSONController.java
+++ LinkedDB/src/Interface_FX/Create_DocJSONController.java
@@ -10,9 +10,44 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class Create_DocJSONController implements Initializable {
+	@FXML private Label textCorrecDocJSON;
+	@FXML private Button cont_Atributos;
+	
+	
+	@FXML
+	public void verificar(ActionEvent event){
+		//if (Existe un JSON Store )
+		textCorrecDocJSON.setText("Proceso de verificación");
+		cont_Atributos.setDisable(false); // Habilita el otro boton para seguir con el proceso
+			
+	}
+	@FXML
+	public void continuar(ActionEvent event){
+		//if (Existe un JSON Store )
+		textCorrecDocJSON.setText("Proceso de verificación");
+		cont_Atributos.setDisable(false);
+		
+			try{
+				((Node)event.getSource()).getScene().getWindow().hide();
+				FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Atribute_List.fxml"));
+				Parent root3 = (Parent) fxmlLoader.load();
+				Stage stage = new Stage();
+				
+				stage.setTitle("LInkedDB");
+				stage.setScene(new Scene(root3));
+				stage.show();
+				
+			}catch (Exception e){
+				System.out.println("Can´t load new window");
+			}
+		
+	}
+	
 	
 	@FXML
 	public void atras(ActionEvent event){
@@ -26,10 +61,16 @@ public class Create_DocJSONController implements Initializable {
 			stage.setScene(new Scene(root1));
 			stage.show();
 		}catch (Exception e){
-		System.out.println("Can´t load new window");
+			System.out.println("Can´t load new window");
 	}}
+	
+	
+	
+	
+	
 	@Override
 	public void initialize(URL url, ResourceBundle rb){
+		cont_Atributos.setDisable(true);
 		
 	}
 	
